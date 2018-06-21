384cf2e9340b2ec74a0209b860c239f7096bb499
bo liu <857437038@qq.com>
2018-06-21 22:45:10 +0800

    增加了订单新增页面 增加了用户新增页面

--- src/main/java/com/yc/ordermanage/user/controller/UserController.java
+++ src/main/java/com/yc/ordermanage/user/controller/UserController.java
@@ -1,15 +1,31 @@
 package com.yc.ordermanage.user.controller;
 
+import com.yc.ordermanage.user.domain.UserVO;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@RequestMapping("/user")
+
 @Controller
+@RequestMapping("/user")
 public class UserController {
 
 	@RequestMapping("/page")
 	public String initUserPage() {
 		return "/user/user";
+	}
+
+	@RequestMapping("/add")
+	public String intiUserAdd(){
+		return "/user/userAdd";
+	}
+
+	@RequestMapping("/query")
+	@ResponseBody
+	public UserVO queryUser(String username){
+		UserVO userVO = new UserVO();
+		userVO.setUserid("sysadmin");
+		return userVO;
 	}
 
 }
