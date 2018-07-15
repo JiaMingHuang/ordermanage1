6ef699b940f256dd5a8ec90956a36c4e9c3245ff
CPPHJJ <Jiaming_Huang@cathaypacific.com>
2018-07-15 20:56:27 +0800

    修改页面功能名称

--- src/main/java/com/yc/ordermanage/base/service/BaseService.java
+++ src/main/java/com/yc/ordermanage/base/service/BaseService.java
@@ -1,13 +1,5 @@
 package com.yc.ordermanage.base.service;
 
-import com.sun.xml.internal.bind.v2.model.core.ID;
-import com.yc.ordermanage.base.dao.BaseRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import java.io.Serializable;
-import java.util.List;
-
 //@Service
 public class BaseService/*<T>*/{
 /*
